2ba48bce14d0a4e9744eebaef73aed9d9a1ad9ab
missnaqvi <imnidanaqvi@hotmail.com>
2019-01-27 14:52:03 +0500

    27th January 2019

--- index.php
+++ index.php
@@ -51,27 +51,149 @@ require_once 'header.php'
 		console.log("Fail");
 	}*/
 
-	var number=33;
-	if (number>=33 && number<=39) {
-		window.alert('Grade E');
-	} else if (number>=40 && number<=49){
-		window.alert('Grade D');
-	} else if (number>=50 && number<=59){
-		window.alert('Grade C');
-	} else if (number>=60 && number<=69){
-		window.alert('Grade B');
-	} else if (number>=70 && number<=79){
-		window.alert('Grade A');
-	} else if (number>=80 && number<=100){
-		window.alert('Grade A+');
-	}
+	// var number=33;
+	// if (number>=33 && number<=39) {
+	// 	window.alert('Grade E');
+	// } else if (number>=40 && number<=49){
+	// 	window.alert('Grade D');
+	// } else if (number>=50 && number<=59){
+	// 	window.alert('Grade C');
+	// } else if (number>=60 && number<=69){
+	// 	window.alert('Grade B');
+	// } else if (number>=70 && number<=79){
+	// 	window.alert('Grade A');
+	// } else if (number>=80 && number<=100){
+	// 	window.alert('Grade A+');
+	// }
 	
 
-	else{
-		window.alert('Fail');
-	}
+	// else{
+	// 	window.alert('Fail');
+	// }
 
-	
+// 	console.log("Welcome to ABC Bank");
+// 	console.log("Please press any options from below");
+// 	console.log("1. Inquiry");
+// 	console.log("2. Fund Transfer");
+// 	console.log("3. Exit");
+
+// var opt=1;
+// switch(opt)
+
+// {
+// case 1:
+
+// console.log("inquiry");
+// break;
+// case 2:
+// console.log("Fund Transfering");
+// break;
+// case 3:
+// console.log("Pls wait for few seconds we are exiting from menu");
+// default:
+// console.log("Invalid input, pleae try another selection");
+// }
+
+
+
+// for(var i = 1; i<=1000; i++)
+
+// {
+// console.log("your incremented value is",i);
+
+// }
+
+// for(var i = -1; i>=-10; i--)
+
+// {
+// console.log("your decremented value is",i);
+
+// }
+
+/*var table=3;
+
+for(var i = 1; i<=10; i++)
+
+{
+console.log(table + "x" + i + "=" + table*i) ;
+
+}*/
+
+ /*var i=10;
+ while(i>=1){
+console.log(i);
+i--
+ }*/
+
+/*var i=0;
+ while(i<=10){
+ 	if (i%2==0)
+console.log(i);
+i++
+ }*/
+
+/*var i=1;
+ while(i<=15){
+ 	if (i%2==1)
+console.log(i);
+i++
+ }*/
+
+ function add(){
+	var a=10;
+	var b=5;
+	var addition=a+b;
+	console.log(addition);
+
+}
+/*add();*/
+
+function sub(){
+	var a=10;
+	var b=5;
+	var subtract=a-b;
+	console.log(subtract);
+
+}
+/*sub();*/
+
+function mul(){
+	var a=10;
+	var b=5;
+	var multiply=a*b;
+	console.log(multiply);
+
+}
+/*mul();
+*/
+
+function div(){
+	var a=10;
+	var b=5;
+	var divide=a/b;
+	console.log(divide);
+
+}
+/*div();*/
+var opt=div;
+switch (opt)
+{
+	case add:
+	add();
+	break;
+	case sub:
+	sub();
+	break;
+	case mul:
+	mul();
+	break;
+	case div:
+	div();
+	break;
+	default:
+	console.log('Invalid Input');
+}
+
 
 </script>
 
--- portfolio.php
+++ portfolio.php
@@ -6,6 +6,26 @@ require_once 'header.php'
 
 <!-- header end -->
 
+<div class="container-fluid img-fluid">
+	<div class="row">
+		<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 img-fluid">
+			<img src="assets/img/port01.jpeg">
+			
+		</div>
+		<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 img-fluid">
+			<!-- Portion 02 -->
+			<img src="assets/img/port02.jpg">
+		</div>
+		<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 img-fluid">
+			<!-- portion 03 -->
+			<img src="assets/img/port03.jpeg">
+		</div>
+	</div>
+
+
+</div>
+
+
 
 
 <!-- footer start -->
