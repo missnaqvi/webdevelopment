2745933bc66295964f5195055b0bbe914ec73a90
missnaqvi <imnidanaqvi@hotmail.com>
2019-02-02 01:12:06 +0500

    port 2nd row

--- portfolio.php
+++ portfolio.php
@@ -21,7 +21,20 @@ require_once 'header.php'
 			<img src="assets/img/port03.jpeg" class="img-fluid">
 		</div>
 	</div>
-
+<div class="row">
+		<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 img-fluid port">
+			<img src="assets/img/port03.jpeg" class="img-fluid">
+			
+		</div>
+		<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 port">
+			<!-- Portion 02 -->
+			<img src="assets/img/port01.jpeg" class="img-fluid">
+		</div>
+		<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 port">
+			<!-- portion 03 -->
+			<img src="assets/img/port02.jpg" class="img-fluid">
+		</div>
+	</div>
 
 </div>
 
